60c4e661dbf25c94be7239083285378ba0c3d2b9
JosueC07183 <josue.salmeron@ucr.ac.cr>
2023-05-05 00:03:21 -0600

    Comentarios template & main

--- Templates/stackTemplate.cpp
+++ Templates/stackTemplate.cpp
@@ -23,22 +23,62 @@ OTRO MODO, QUE SURJA DE, FUERA DE O EN CONEXIÓN CON EL SOFTWARE O EL USO U
 OTROS ACUERDOS EN EL SOFTWARE.
 */
 
-#include <iostream>
-#include <vector>
-#include <algorithm>
-#include <stdexcept>
-#include <functional>
+/**
+ * @file stackTemplate.cpp
+ * @author Josué Salmerón Córdoba (tomado del repositorio ebadilla10)
+ * @brief La idea de esta segunda parte de la tarea es identificar que headers no son necesarios para el 
+ * código implementado. Y en efecto, hay headers inútiles en este archivo. Lo que se hizo fue instalar una 
+ * extensión de C++ en Vscode y cuando se comentaba cualquier header, entonces vscode de manera inmediata
+ *  marca en rojo la instrucción que depende de esa librería, el hacer esto facilitó el trabajo solicitado.
+ * De esa manera, los headers innecesarios son algorithm y vector. Los demás si se ocupan para trabjar con 
+ * algunas funciones específicas.
+ * 
+ * @version 0.1
+ * @date 2023-05-04
+ * 
+ * @copyright Copyright (c) 2023
+ * 
+ */
 
+#include <iostream>
+//#include <vector>
+// #include <algorithm>
+#include <stdexcept> // se queja el typename y la excepción.
+#include <functional> // se queja el foreach.
+/**
+ * @brief Acá se implementa un template que inicialmente muestra el tamaño de la pila. Que es de 3
+ * es un atributo privado para que no se pueda editar. Luego, la función push toma el typename de 
+ * la clase Stack que se encarga de añadir un elemento al final del vector. 
+ * 
+ * @tparam T 
+ */
 template<typename T>
 class Stack {
 private:
   std::vector<T> data_;
-
+/**
+ * @brief Se imprime el valor de la pila.
+ * 
+ */
 public:
+/**
+ * @brief Se añade un elemento al final del vector.
+ * 
+ * @param value: int que almacena el valor de la pila.
+ */
   void push(T value) {
     data_.push_back(value);
   }
+/**
+ * @brief Después se toma a T, que elimina el primer elemento 
+ * de la cola, por eso es que se usa un condicional ya que se hace uso de empty() y devolver true si el vector está 
+ * vacío o false caso contrario. Solo que en este caso se usa throw, y esto sirve para lanzar una excepción si se 
+ * detecta un problema. Fuera del condicional T value devuelve una referencia al último elemento de la lista y al 
+ * mismo tiempo se elimina el último elemento.
 
+ * 
+ * @return T 
+ */
   T pop() {
     if (data_.empty()) {
       throw std::out_of_range("Stack is empty");
@@ -47,24 +87,51 @@ public:
     data_.pop_back();
     return value;
   }
-
+/**
+ * @brief La función void clear, elimina todos los elementos.
+ * 
+ */
   void clear() {
     data_.clear();
   }
-
+/**
+ * @brief Acá básicamente devolverá un true si la lista está vacía, un false caso
+ * contrario.
+ * 
+ * @return true 
+ * @return false 
+ */
   bool empty() const {
     return data_.empty();
   }
-
+/**
+ * @brief En esta pequeña función se muestra el tamaño de la lista 
+ * Eso justifica el uso de .size().
+ * @return std::size_t: tamaño de la lista.
+ */
   std::size_t size() const {
     return data_.size();
   }
-
+/**
+ * @brief Esta función no devuelve ningún valor, ya que es un void. Toma la referencia sobre T.
+ * y los compara con la referencia func. Luego, se imprimen los valores del inicio y el final de la 
+ * pila.
+ * 
+ * @param func: variable enetera del stack usado.
+ */
   void foreach(const std::function<void(T&)>& func) {
     std::for_each(data_.begin(), data_.end(), func);
   }
 };
-
+/**
+ * @brief Esto es el llamado de la función main, aquí se carga la pila, o se apila los elementos que se
+ * van a usar (viéndolos o pensando ensamblador) ya que se hace uso de push, es decir, se están agregando 
+ * elementos al final de la lista, un total de 4. Después se imprime el tamaño de la pila con .size().
+ * Por último se hace un llamado a la función foreach para traer lo que tiene contenido value y mostrarlo
+ * por consola, además se desapila el valor del stack, esto con ayuda de .pop. de donde se atrapan las exce-
+ * pciones del programa con try y catch para estar seguros de no tener valores inapropiados.
+ * @return int 
+ */
 int main() {
   Stack<int> s;
   s.push(2021);
@@ -76,7 +143,10 @@ int main() {
   s.foreach([](int& value) {
     std::cout << "Value: " << value << std::endl;
   });
-
+/**
+ * @brief 
+ * 
+ */
   try {
     while (!s.empty()) {
       int value = s.pop();
